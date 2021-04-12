2c66babf1e0430f0049f86ab106b1d2ede538c37
tmffv <56069000+tmffv@users.noreply.github.com>
2021-04-12 18:13:15 +0300

    update code

--- lab8/lab8.py
+++ lab8/lab8.py
@@ -1,37 +1,32 @@
 import numpy as np
 import scipy.stats as stats
 
-confidence_level = 0.95
-alpha = 1 - confidence_level
-nums = [20, 100]
-
-
-def o(x):
-    return np.around(x, decimals=2)
+ALPHA = 0.05
+N_SIZE = [20, 100]
 
 
 def muConfidenceInterval(sample):
     mu = np.mean(sample)
     sigma = np.std(sample)
     n = len(sample)
-    ci = sigma * stats.t.ppf(1 - alpha/2, n - 1) / np.sqrt(n - 1)
-    return o(mu - ci), o(mu + ci)
+    ci = sigma * stats.t.ppf(1 - ALPHA/2, n - 1) / np.sqrt(n - 1)
+    return np.around(mu - ci, decimals=2), np.around(mu + ci, decimals=2)
 
 
 def sigmaConfidenceInterval(sample):
     sigma = np.std(sample)
     n = len(sample)
-    ci_start = sigma * np.sqrt(n / stats.chi2.ppf(1 - alpha/2, n - 1))
-    ci_end = sigma * np.sqrt(n/stats.chi2.ppf(alpha/2, n - 1))
-    return o(ci_start), o(ci_end)
+    ci_start = sigma * np.sqrt(n / stats.chi2.ppf(1 - ALPHA/2, n - 1))
+    ci_end = sigma * np.sqrt(n/stats.chi2.ppf(ALPHA/2, n - 1))
+    return np.around(ci_start, decimals=2), np.around(ci_end, decimals=2)
 
 
 def muConfidenceIntervalAsymptotic(sample):
     mu = np.mean(sample)
     sigma = np.std(sample)
     n = len(sample)
-    ci = sigma * stats.norm.ppf(1 - alpha/2) / np.sqrt(n)
-    return o(mu - ci), o(mu + ci)
+    ci = sigma * stats.norm.ppf(1 - ALPHA/2) / np.sqrt(n)
+    return np.around(mu - ci, decimals=2), np.around(mu + ci, decimals=2)
 
 
 def sigmaConfidenceIntervalAsymptotic(sample):
@@ -39,16 +34,16 @@ def sigmaConfidenceIntervalAsymptotic(sample):
     n = len(sample)
     m4 = stats.moment(sample, 4)
     e = m4 / pow(float(sigma), 4) - 3
-    U = stats.norm.ppf(1 - alpha/2) * np.sqrt((e + 2)/n)
+    U = stats.norm.ppf(1 - ALPHA/2) * np.sqrt((e + 2)/n)
     ci_start = sigma / np.sqrt(1 + U)
     ci_end = sigma / np.sqrt(1 - U)
-    return o(ci_start), o(ci_end)
+    return np.around(ci_start, decimals=2), np.around(ci_end, decimals=2)
 
 
 if __name__ == "__main__":
-    for num in nums:
+    for num in N_SIZE:
         sample = np.random.normal(0, 1, size=num)
-        print('Size = ', num)
+        print('N = ', num)
         print('Mean', muConfidenceInterval(sample))
         print('Variance', sigmaConfidenceInterval(sample))
         print('Asymptotic_mean', muConfidenceIntervalAsymptotic(sample))
